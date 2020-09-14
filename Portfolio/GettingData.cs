86631a506a96245dbf9401607b78eb9787d234f1
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2020-09-14 12:27:21 +1100

    Feature prep: added class for Stock with history (enabler for balancing)

--- Portfolio/GettingData.cs
+++ Portfolio/GettingData.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.Xml;
 using System.Threading.Tasks;
 
 namespace MOEX_web.Portfolio
@@ -20,8 +22,67 @@ namespace MOEX_web.Portfolio
             Value = value;
         }
     }
-        
 
+    public class StockRecord
+    {
+        public DateTime Date { get; private set; }
+        public double Price { get; set; }
+
+        public StockRecord(DateTime date, double price)
+        {
+            Date = date;
+            Price = price;
+        }
+
+    }
+    public static class RecordExtensions
+    {
+        public static void SortRecords(this List<StockRecord> records)
+        {
+            records.Sort((rec1, rec2) => rec1.Date.CompareTo(rec2.Date));
+        }
+
+    }
+
+    public class StockWithHistory : Stock
+    {
+        public List<StockRecord> History { get; private set; } = new List<StockRecord>();
+
+        public StockWithHistory(string name, double value) : base(name, value) { }
+        public StockWithHistory(Stock stock) : base(stock.Name, stock.Value)
+        {
+            if (stock.StartPrice > 0) History.Add(new StockRecord(stock.StartDate, stock.StartPrice));
+            if (stock.EndPrice > 0) History.Add(new StockRecord(stock.EndDate, stock.EndPrice));
+        }
+
+
+        public void SortDates()
+        {
+            History.SortRecords();
+
+            StartDate = History[0].Date;
+            StartPrice = History[0].Price;
+            EndDate = History[^1].Date;
+            EndPrice = History[^1].Price;
+        }
+
+        public void AddDate(DateTime date)
+        {
+            History.Add(new StockRecord(date, 0));
+            History.SortRecords();
+        }
+
+        public async Task GetPricesAsync(bool onlyForEmpty = true)
+        {
+            //var runningRequests = new List<Task>();
+            foreach (var record in History)
+            {
+                if (onlyForEmpty && record.Price <= 1) record.Price = await GettingData.GetStockDataAsync(Name, record.Date);
+                if (!onlyForEmpty) record.Price = await GettingData.GetStockDataAsync(Name, record.Date);
+            }
+        }
+
+    }
     public class GettingData
     {
         public static async Task<double> GetStockDataAsync(string stockName, DateTime date)
@@ -32,7 +93,7 @@ namespace MOEX_web.Portfolio
             try
             {
                 string requestHTTP = $"history/engines/stock/markets/shares/securities/{stockName}.json?from={fromDate:yyyy-MM-dd}&till={tillDate:yyyy-MM-dd}";
-                Console.WriteLine(new Uri(client.BaseAddress, requestHTTP)); 
+                Console.WriteLine(new Uri(client.BaseAddress, requestHTTP));
                 var response = await client.GetAsync(requestHTTP);
                 if (response.IsSuccessStatusCode)
                 {
@@ -59,6 +120,7 @@ namespace MOEX_web.Portfolio
             stock.EndDate = date;
             stock.EndPrice = await GetStockDataAsync(stock.Name, date);
         }
+
 
         // public class Wallet
         // {
